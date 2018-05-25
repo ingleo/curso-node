db1d1c27f187704a3091141fff8f822169b55299
ingleo <clgrhc@gmail.com>
2018-05-25 05:47:51 -0500

    changing code test

--- 03-Bases-Node/multiplicar/multiplicar.js
+++ 03-Bases-Node/multiplicar/multiplicar.js
@@ -2,18 +2,15 @@ const fs = require('fs');
 
 crearArchivo = (base) => {
     return new Promise((resolve, reject) => {
+        let data = '';
 
+        for (let i = 1; i <= 10; i++) {
+            data += `${base} * ${i} = ${base*i}\n`;
+        }
+
+        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+            if (err) throw err;
+            console.log('Archivo creado');
+        });
     });
 };
-
-
-let data = '';
-
-for (let i = 1; i <= 10; i++) {
-    data += `${base} * ${i} = ${base*i}\n`;
-}
-
-fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
-    if (err) throw err;
-    console.log('Archivo creado');
-});
